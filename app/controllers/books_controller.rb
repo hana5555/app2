c8e366e0fabe72dd71f3f6bdd72f1490eef2e493
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-07-22 13:28:25 +0000

    [Add]home,about,users,books各ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,59 @@
 class BooksController < ApplicationController
-  def new
-  end
-  
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.new
+    @books = Book.find(params[:id])
+    @user = @books.user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
+    @books = Book.find(params[:id])
+    @books.destroy
+    flash[:danger] = "削除しました"
+    redirect_to books_path
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
 end
