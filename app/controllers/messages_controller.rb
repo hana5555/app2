36d2d113c95406e664215935fc8f64c05409bb52
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-08-15 10:25:13 +0000

    [Finish]DM
    機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,19 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!, :only => [:create]
 
   def create
-    if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:message,:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
+    message = Message.new(message_params)
+    message.user_id = current_user.id
+    if message.save
+      redirect_to room_path(message.room)
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+  
+  private
+  
+  def message_params
+    params.require(:message).permit(:room_id, :message)
   end
 
 end
