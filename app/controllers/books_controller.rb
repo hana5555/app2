7fb8238a31223c1435a7b573f325460ac7d3384e
HanaHarada <129660325+hana5555@users.noreply.github.com>
2023-08-18 09:30:56 +0000

    [Add]閲覧数カウント途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,11 @@ class BooksController < ApplicationController
     @books = Book.find(params[:id])
     @user = @books.user
     @book_comment = BookComment.new
+
+    @book_detail = Book.find(params[:id])
+    unless ReadCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.read_counts.create(book_id: @book_detail.id)
+    end
   end
 
   def edit
